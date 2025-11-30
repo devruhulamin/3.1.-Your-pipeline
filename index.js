5fcf0d28719b502bb9082a194aba336e80d80702
Ruhul Amin <pythonruhul@gmail.com>
2025-11-30 11:48:06 +0600

    fix issue

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hi MOOC</h1>')
+  res.send(`
+  <style>
+    body {
+      margin:0; height:100vh; 
+      background:#006a4e; 
+      display:grid; place-items:center; 
+      font-family:Arial;
+    }
+    h1 {
+      color:#f42a41; 
+      font-size:4rem; 
+      text-shadow:0 0 20px white;
+    }
+  </style>
+  <h1>BANGLADESH</h1>
+`)
 })
 
 const PORT = 8080
